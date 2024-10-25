805606b6cf27d0f314e9bf6f8d2226026e1c81b0
Jheremy.Conca <concajheremy@gmail.com>
2024-10-25 01:44:51 -0500

    Actualiza configuraciones y mejora conexión a la base de datos

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
+export const DB_USER = process.env.DB_USER;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_DATABASE = process.env.DB_DATABASE;
+export const DB_PORT = process.env.DB_PORT;
+
 export const PORT = process.env.PORT || 4000;
-app.listen(PORT, () =>{
-    console.log("Server activo en el puerto", PORT)
-})
-export const DB_USER = process.env.DB_USER
-export const DB_HOST = process.env.DB_HOST
-export const DB_PASSWORD = process.env.DB_PASSWORD
-export const DB_DATABASE = process.env.DB_DATABASE
-export const DB_PORT = process.env.DB_PORT
+
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import { PORT } from "./config.js";
 import userRoutes from "./routes/users.routes.js";
 import dinoRoutes from "./routes/dino.routes.js";
 import favRoutes from "./routes/fav.routes.js";
+import morgan from 'morgan';
+import dotenv from 'dotenv';
 
-import morgan from 'morgan'
+
 
 const app = express();
 
@@ -14,5 +16,8 @@ app.use(userRoutes);
 app.use(dinoRoutes);
 app.use(favRoutes);
 
-app.listen(PORT);
-console.log("Server on port", PORT);
+app.listen(PORT, () => {
+    console.log("Server on port", PORT);
+});
+// Carga las variables de entorno
+dotenv.config();
