7b8ff675154e4086938e164ab654d185a6a2f778
Jheremy.Conca <concajheremy@gmail.com>
2024-10-25 01:08:33 -0500

    api rest

--- src/db.js
+++ src/db.js
@@ -4,15 +4,15 @@ import {
     DB_DATABASE, 
     DB_HOST, 
     DB_PASSWORD, 
-    DB_PORT, 
-    DB_USER } from './config.js'
+    DB_USER, 
+    DB_PORT} from './config.js'
 
 export const pool = new pg.Pool({
     user: DB_USER,
     host: DB_HOST,
     password: DB_PASSWORD,
     database: DB_DATABASE,
-    port: PORT
+    port: DB_PORT
 })
 pool.query('SELECT NOW()', (err, res) => {
     if (err) {
